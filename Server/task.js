d114f188b1f699a2bf7a2c60956de5110d6d7322
Vinay <Vinay.bv@accionlabs.com>
2017-06-02 11:09:51 +0530

    init

---
+++ Server/task.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var spawn = require('child_process').spawn;
+var livereload = require('gulp-livereload');
+
+
+var gulpActivity = function(app, http) {
+
+	this.app = app;
+	this.http = http;
+
+	var sass_files  = [
+	               'sass/*',
+	               'sass/base/*',
+	               'sass/utitlities/*',
+	               'sass/test/*'
+	               ];
+	var css = ['css/*'];
+	var html = [
+	               'html/*'
+	            ];
+
+	this.start = function(port) {
+
+		
+
+		gulp.task('connect', function() {
+				   http.listen(port, function(){
+				   console.log('listening on :' + port);
+				});
+		});
+
+		gulp.task('sass', function() {
+			return gulp.src('sass/main.sass')
+			    .pipe(sass().on('error', sass.logError))
+    			.pipe(gulp.dest('./css'));
+		})
+
+		gulp.task('watch', function(event) {
+			//livereload.listen();
+			gulp.watch(sass_files, ['sass']);
+			gulp.watch(html, ['gulp-autoreload']);
+		})
+
+      
+       gulp.task('gulp-autoreload', function() {
+			spawn('gulp', ['default'], {stdio: 'inherit'});
+            process.exit();
+      });
+
+
+
+		gulp.task('default', ['connect', 'sass', 'watch']);
+
+
+	}
+	
+}
+module.exports = gulpActivity;
+
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+app.use(cors());
+var http = require('http').Server(app);
+var path = require('path');
+var gulp_task = require('./Server/task');
+var Server = new gulp_task(app, http);
+var bodyParser = require('body-parser');
+var port = 3000;
+
+
+
+// app.set('views', path.join(__dirname, 'ui'));
+// // app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'sass')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/css', function(req, res){
+  res.sendFile(__dirname + '/css/main.css');
+});
+
+
+
+
+
+Server.start(port);
+
+
+
